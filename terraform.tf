1f8b75e0e133959824d2ccdd7067f73c868ef5e1
ulfjoh <ulf.k.johansson@gmail.com>
2025-07-24 15:18:22 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "04-06-ci-cd-for-iac"             # the bucket
+    region = "us-east-1"             # the region
   }
 }
